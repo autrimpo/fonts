d4c0e7db8d23b764dfaf02894f98469ffeb1e786
Michal Koutenský <michal@koutensky.net>
2023-11-10 23:36:18 +0100

    feat: add indian type foundry

---
+++ pkgs/indianTypeFoundry/default.nix
@@ -0,0 +1,33 @@
+{
+  mkFont,
+  lib,
+  fetchzip,
+}: let
+  version = import ./_version.nix;
+  fetchFontshare = {
+    pname,
+    sha256,
+  }:
+    fetchzip {
+      url = "https://api.fontshare.com/v2/fonts/download/${pname}";
+      inherit sha256;
+    };
+  mkFontshare = {
+    pname,
+    sha256,
+  }:
+    mkFont {
+      inherit pname version;
+      src = fetchFontshare {inherit pname sha256;};
+      preInstall = ''
+        find -iname 'web' -type d -print0 | xargs -0 -r rm -rf
+      '';
+    };
+in
+  lib.mapAttrs
+  (name: value:
+    mkFontshare {
+      pname = name;
+      sha256 = value;
+    })
+  (import ./_sources.nix)
